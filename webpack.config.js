8fcfd041665c0f496584828fa577efbbfcad74a8
Debabhiram Manik <debu.manik5431@gmail.com>
2023-01-07 09:43:55 +0530

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'client/src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+};
